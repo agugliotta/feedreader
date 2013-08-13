1d1d93ab8c7a33185efe60cff039f08efde1331b
agugliotta <agustin.mdq.89@gmail.com>
2013-08-13 12:07:24 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Feedreader::Application.config.secret_key_base = '273dd231112e028f573d02cb0271019b655c21047f3332f004b69d2b578658d2a16b418365d6abf7a0fe95a14998dfc691782dc0e3ddc172e636b78cc02656b4'
